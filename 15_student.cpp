b0a24d5d437729ec13764b9072c9a7600e74b7a9
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-12-09 16:03:54 +0530

    12/09

--- 15_student.cpp
+++ 15_student.cpp
@@ -8,6 +8,7 @@ class student
   static int totalpass;
   static int totalfail;
   static int totalcount;
+  static string class_teacher;
 
 public:
   void setstudent(string s1, int a, float b)
@@ -35,28 +36,47 @@ public:
     }
     cout << "_____________________________________" << endl;
   }
-  void totalresult()
+  static void totalresult()
   {
     cout << "total studentd of class : " << totalcount << endl;
     cout << "total pass student : " << totalpass << endl;
     cout << "total fail student : " << totalfail << endl;
     cout << "_____________________________________" << endl;
   }
+  static void disp_class_teacher()
+  {
+    cout << "class Teacher name : " << class_teacher << endl;
+    cout << "_____________________________________" << endl;
+  }
+  static void change_class_teacher(string s)
+  {
+    class_teacher = s;
+  }
+  static void classinfo()
+  {
+    cout << "inside class we have 6 methods" << endl;
+    cout << "thanks for using our class" << endl;
+  }
 };
 int student::totalpass = 0;
 int student::totalfail = 0;
 int student::totalcount = 0;
+string student::class_teacher = "teena sharma";
 int main()
 {
-  student s1, s2, s3,s4;
+  student s1, s2, s3, s4;
   s1.setstudent("ram", 101, 45);
   s2.setstudent("shyam", 102, 23);
   s3.setstudent("rohit", 103, 12);
-  s4.setstudent("aman", 104,4);
+  s4.setstudent("aman", 104, 4);
+  student::disp_class_teacher();
   s1.displayresult();
   s2.displayresult();
+  student::change_class_teacher("mahak gupta");
+  student::disp_class_teacher();
   s3.displayresult();
   s4.displayresult();
-  s1.totalresult();
+  student::totalresult();
+  student::classinfo();
   return 0;
 }
